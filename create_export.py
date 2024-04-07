33264f4071b6ed32e3d956602cabaf5ab1ccfefb
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-04-07 18:54:56 +0300

    feat: use kuzu schema  locations

--- create_export.py
+++ create_export.py
@@ -5,4 +5,4 @@ files = [f.name.split('.')[0] for f in pathlib.Path().glob("../imdb_dataset_arti
 print("USE main;")
 for filename in files:
     #print(f"COPY (SELECT * FROM {filename}) TO 'C:/work\neo4j_imdb_dataset/export_elt/{filename}.csv.gz'(HEADER, DELIMITER ';', COMPRESSION 'gzip');")
-    print(f"COPY (SELECT * FROM {filename}) TO 'C:/work\kuzu_imdb_dataset/export_elt/{filename}.parquet' (FORMAT PARQUET);")
+    print(f"COPY (SELECT * FROM main.imdb_kuzu.{filename}) TO 'C:/work/imdb_graphdb_article/export_elt/{filename}.parquet' (FORMAT PARQUET);")
