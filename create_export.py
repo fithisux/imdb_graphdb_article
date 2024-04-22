90178309724ff76cbb8d727609039f033d3196fd
Vasileios Anagnostopoulos <vasilis.anagnostopoulos@agileactors.com>
2024-04-22 18:24:50 +0300

    feat: notebook now does the export too

--- create_export.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-
-files = [f.name.split('.')[0] for f in pathlib.Path().glob("../imdb_dataset_article/models/imdb_kuzu/*.sql")]
-
-print("USE main;")
-for filename in files:
-    #print(f"COPY (SELECT * FROM {filename}) TO 'C:/work\neo4j_imdb_dataset/export_elt/{filename}.csv.gz'(HEADER, DELIMITER ';', COMPRESSION 'gzip');")
-    print(f"COPY (SELECT * FROM main.imdb_kuzu.{filename}) TO 'C:/work/imdb_graphdb_article/export_elt/{filename}.parquet' (FORMAT PARQUET);")
